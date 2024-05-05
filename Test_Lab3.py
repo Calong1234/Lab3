360ec6d952d2eb24daa47ad7a71f1f945f627c95
CALEB <CALEB.23@ichat.sp.edu.sg>
2024-05-05 16:50:08 +0800

    added Test_price_info.py file

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -9,7 +9,7 @@ def test_bubble_sort_ascending():
     test_arr = [11, 12, 22, 25, 34, 64, 90]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
-
+    print(result)
     assert (result == test_arr)
 
 def test_bubble_sort_descending():
@@ -21,10 +21,23 @@ def test_bubble_sort_descending():
 
     assert (result == test_arr)
 
-def test_bubble_sort_invalid():
-    result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
+def test_bubble_sort_more_than_10():
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 19, 56, 25, 15]
 
-    result = Lab3.bubble_sort(input_arr, 3)
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
 
-    assert (result == [])
+    assert (result == 1)
+
+def test_bubble_sort_0():
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 0)
+
+def test_bubble_sort_non_integer():
+    input_arr = [64, 34, 25, 12, 22.2, 11, 90]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 2)
